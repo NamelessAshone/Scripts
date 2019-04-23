1ebabe02be5fd63b7cfc44320868a59579d57563
psalm46 <1057803692@qq.com>
2019-04-23 13:50:57 +0800

    add Shell/git proxy scripts and polipo config file for Shell proxy

---
+++ AcrossGFW/closeProxy.sh
@@ -0,0 +1,3 @@
+unset http_proxy
+unset https_proxy
+git config --global --unset https.proxy
---
+++ AcrossGFW/openProxy.sh
@@ -0,0 +1,3 @@
+export http_proxy=http://127.0.0.1:8123
+export https_proxy=https://127.0.0.1:8123
+git config --global https.proxy https://127.0.0.1:8123
