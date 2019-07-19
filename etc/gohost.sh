3e1166b50a27f714a13223f4e8277726faede113
Psalm46 <1057803692@qq.com>
2019-07-19 16:57:31 +0800

    [修改] 添加了script.pl和.gitcofig 修改了gohost.sh

--- etc/gohost.sh
+++ etc/gohost.sh
@@ -1,1 +1,31 @@
+#! /usr/bin/env expect
+
+set HOST    [lindex $argv 0]
+
+set P   "1qaz@WSX"
+
+if {[llength $HOST] == 0} {
+    send_user "Usage: gohost <HOST>\n"
+    send_user "Avaible host list:\n\tinformix\n\tismp\n"
+}
+
+spawn ssh $HOST
+sleep 1
+send_user $P
+expect {
+    timeout      {
+        send_user "timeout\n";
+        exit 1
+    }
+    "*password*"   {
+        send "$P\r\n";
+        sleep 1
+        expect {
             timeout {
+                send_user "timeout\n"
+                exit 1
+            }
+            "*$*" interact
+        }
+    }
+}
